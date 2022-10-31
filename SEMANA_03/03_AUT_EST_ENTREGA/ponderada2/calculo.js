61a2459e19b22a8d52ea2a57219d3612213e4150
lfCovas <luiz.covas@sou.inteli.edu.br>
2022-10-31 14:25:50 -0300

    Change ATV3

--- SEMANA_03/03_AUT_EST_ENTREGA/ponderada2/calculo.js
+++ SEMANA_03/03_AUT_EST_ENTREGA/ponderada2/calculo.js
@@ -1,34 +1,34 @@
 function calculaVel0(){
     var vel0 = parseFloat (document.getElementById('V0').value);
-    document.getElementById('saida').innerHTML = vel0 / 10 ;
+    document.getElementById('saida').innerHTML = "Resultado: " + (vel0 / 10) + "s" ;
 }
 
 function calculaHmax(){
     var vel0 = parseFloat (document.getElementById('V0').value);
-    document.getElementById('resultado').innerHTML = (vel0*vel0) / (2*10) ;
+    document.getElementById('resultado').innerHTML = "Resultado: " + (vel0*vel0) / (2*10) + "m";
 }
 
 function calculaXv(){
     var va = parseFloat (document.getElementById('a').value);
     var vb = parseFloat (document.getElementById('b').value);
-    document.getElementById('resultadoXv').innerHTML = (-(vb/(2*va)));
+    document.getElementById('resultadoXv').innerHTML = "Resultado: " + (-(vb/(2*va)));
 }
 
 function calculaYv(){
     var va = parseFloat (document.getElementById('a').value);
     var vb = parseFloat (document.getElementById('b').value);
     var vc = parseFloat (document.getElementById('c').value);
-    document.getElementById('resultadoYv').innerHTML = (((((-vb*(-vb))-4*(va*vc))/(4*va))));
+    document.getElementById('resultadoYv').innerHTML = "Resultado: " + (((((-vb*(-vb))-4*(va*vc))/(4*va))));
 }
 
 function changeToCelcius(){
     var tf = parseFloat (document.getElementById('temperatura').value);
-    document.getElementById('resultadoTc').innerHTML = (((tf-32) / 9)*5);
+    document.getElementById('resultadoTc').innerHTML = "Resultado: " + (((tf-32) / 9)*5) + "°C";
 }
 
 function changeToFahrenheit(){
     var tc = parseFloat (document.getElementById('temperatura').value);
-    document.getElementById('resultadoTf').innerHTML = ((tc/5)*9+32);
+    document.getElementById('resultadoTf').innerHTML = "Resultado: " + ((tc/5)*9+32) + "°F";
 }
     
 function totalPagar(){
@@ -38,13 +38,13 @@ function totalPagar(){
     
     if(Kwh > 100 && Kwh <= 200){
         var resultado = (resultado + (resultado*0.25));
-        document.getElementById('resultadoConta').innerHTML = resultado;
+        document.getElementById('resultadoConta').innerHTML = "Total a pagar: " + (resultado);
     }else if(Kwh > 200){
        var resultado = (resultado + (resultado*0.50));
-        document.getElementById('resultadoConta').innerHTML = resultado;
+        document.getElementById('resultadoConta').innerHTML = "Total a pagar: " + (resultado);
     }else{
         var resultado = resultado;
-        document.getElementById('resultadoConta').innerHTML = resultado;
+        document.getElementById('resultadoConta').innerHTML = "Total a pagar: " + (resultado);
     }
 
 }
