e9579c9421e12f7cf8d0de9a99e32ff6ed3871e1
lfCovas <luiz.covas@sou.inteli.edu.br>
2022-11-24 16:05:18 -0300

    ATVS6

---
+++ SEMANA_05/03_AUT_EST_ENTREGA/jquery.js
@@ -0,0 +1,23 @@
+$('#btn').on("click", deletar)
+
+function deletar(){
+    $('.hr_08').remove()
+    $('.font').remove()
+}
+
+$('#btn2').on("click", excluir)
+
+function excluir(){
+    $('.fotominha').remove()
+}
+
+$('#btn3').on('click', mudarcor)
+
+function mudarcor(){
+    $('body').css("color","rgb(0, 153, 41)")
+    $('.container').css("box-shadow"," 0 0 5px rgb(0, 153, 41)")
+    $('#btn').css("color","rgb(0, 153, 41)")
+    $('#btn2').css("color","rgb(0, 153, 41)")
+    $('#btn3').css("color","rgb(0, 153, 41)")
+    
+}
